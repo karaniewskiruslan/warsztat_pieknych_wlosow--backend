8ff4b0016bf835d6c445d1b165b67d72a6dbea21
Rusłan Karaniewski <karane88vka@gmail.com>
2026-04-11 13:25:49 +0200

    chore: .gitignore update

--- dist/src/helpers/getImageUrl.js
+++
@@ -1,5 +0,0 @@
-export const getImageUrl = (req, imageName) => {
-    return imageName.startsWith(req.protocol)
-        ? imageName.replaceAll('\\', '/')
-        : `${req.protocol}://${req.get('host')}/${imageName}`.replaceAll('\\', '/');
-};
--- dist/src/helpers/responseService.js
+++
@@ -1,5 +0,0 @@
-import { getImageUrl } from './getImageUrl';
-export const responseService = (req, arr) => {
-    const finalList = arr.map((el) => ({ ...el, image: getImageUrl(req, el.image) }));
-    return finalList;
-};
